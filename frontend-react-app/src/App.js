42c66592d220917c8e1a63f6c349904a7a84e0cb
Artem Alieinikov <v-aralie@microsoft.com>
2020-06-02 14:02:56 +0300

    Latest updates were pushed

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, new Docker image
+          Hello, new Docker container!
         </p>
         <a
           className="App-link"
